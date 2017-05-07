d0ee6c40ec37016913118bbf6678891b10755a54
DavidZibler <strangedave11@gmail.com>
2017-05-07 14:46:14 +0200

    Programmieraufgabe
    Aufgabe_1

--- Programmieraufgabe/Aufgabe_1.js
+++ Programmieraufgabe/Aufgabe_1.js
@@ -1,13 +1,4 @@
-/**
-  * @desc fs-Modul in Node.js ermöglicht Zugriff auf Dateien. fs =file system
-  */
-var dirname = dirname;
 var fs = require('fs'); //
-
-/**
-  * @desc: Asynchrone Auslesen von Dateien und Umwandlung von Json-Strings in Objekte
-  *
-*/
 
 fs.readFile(__dirname + "/staedte.json", function(err, data) {
   JSON.parse(data).cities.forEach(function(city) {
